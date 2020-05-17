964f02a78d8da975114c1a767bcb19a99054efff
Christopher Pierce <chris@chris-pierce.com>
2020-05-17 01:14:21 -0400

    adds working mockup of PPT100

--- pfeiffer_vacuum_protocol/mock.py
+++ pfeiffer_vacuum_protocol/mock.py
@@ -59,11 +59,105 @@ class PPT100:
     """
     def __init__(self, address=1, err_state=NO_ERROR):
         self.address = address
-        self.software_version = "010100"
-        self.component = "    A3"
-        self.pressure = 1 # in bar
-        self.pressure_setpoint = 1 # in bar
         self.err_state = err_state
 
     def get_response(self, bin_str):
-        pass
+        # Convert to a unicode str
+        in_str = bin_str.decode("ascii")
+
+        # If it doesn't end in a carriage return, exit
+        if(in_str[-1] != '\r'):
+            return None
+
+        # Validate the checksum and exit if bad
+        if(int(in_str[-4:-1]) != (sum([ord(x) for x in in_str[:-4]])%256)):
+            return None
+
+        # Get the address, and exit if it isn't correct
+        if(int(in_str[:3]) != 1):
+            return None
+
+        # Get the data length and return if it's wrong
+        if(len(in_str)-14 != int(in_str[8:10])):
+            return None
+
+        # Get the operation type (read/write)
+        op_type = int(in_str[3])
+
+        # Get the paramter number
+        param_num = int(in_str[5:8])
+
+        # If we are reading
+        if(op_type == 0):
+            # Check that the data length is right and exit if not
+            if(int(in_str[8:10]) != 2):
+                return None
+
+            # Check that the data is =? exit if it isn't
+            if(in_str[10:12] != "=?"):
+                return None
+
+            # If it is error code, return the right one
+            if(param_num == 303):
+                if(self.err_state == NO_ERROR):
+                    return b'0011030306000000014\r'
+
+                elif(self.err_state == DEFECTIVE_TRANSMITTER):
+                    return b'0011030306Err001168\r'
+
+                elif(self.err_state == DEFECTIVE_MEMORY):
+                    return b'0011030306Err002169\r'
+
+                else:
+                    raise ValueError("unknown error state")
+
+            # Or, if it's version number, return it
+            elif(param_num == 312):
+                return b'0011031206010100016\r'
+
+            # Or, if it's the component name, return it
+            elif(param_num == 349):
+                return b'0011034906    A3236\r'
+
+            # Or, if it's pressure, return it
+            elif(param_num == 740):
+                return b'0011074006100023025\r'
+
+            # Or, if it's the correction value, return it
+            elif(param_num == 742):
+                return b'0011074206000100022\r'
+
+            # If it wasn't any of these, return the error code
+            else:
+                resp = "00110{:03d}06NO_DEF".format(param_num)
+                resp += "{:03d}\r".format(sum([ord(x) for x in resp])%256)
+                return resp.encode()
+
+        # Or, if it's write
+        else:
+            # If it was set point
+            if(param_num == 741):
+                # Check the datatype
+                if(int(in_str[8:10]) != 3):
+                    return b'0011074106NO_DEF191\r'
+
+                # Check bounds and return error if we're out
+                val = int(in_str[10:13])
+                if(val<0 or val>1):
+                    return b'0011074106_RANGE192\r'
+
+                # Return the confirmation
+                return in_str.encode()
+
+            # Or, if it was the correction value
+            elif(param_num == 742):
+                # Check the datatype
+                if(int(in_str[8:10]) != 6):
+                    return b'0011074206NO_DEF192\r'
+
+                # Return the confirmation
+                return in_str.encode()
+
+            # If it wasn't a valid parameter, return an error
+            else:
+                return b'0011074206NO_DEF192\r'
